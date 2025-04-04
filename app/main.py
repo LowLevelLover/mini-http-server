a9abd81d509f9482ad347fa8d173cc1d9ee0e81d
Farzin Vatani <farzinvatani@outlook.com>
2025-04-04 14:25:28 +0330

    Send 200 OK response to client

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    client, ip = server_socket.accept()  # wait for client
+
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    client.sendall(response)
 
 
 if __name__ == "__main__":
