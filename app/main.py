bd22d46e2966ba9e1dd2ff81ce3d4162b6c01bc5
Farzin Vatani <farzinvatani@outlook.com>
2025-04-07 01:23:53 +0330

    Implement concurrency to support multi-clients connections

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
+import asyncio
 import socket
+import logging
+
 from collections import UserDict
 from typing import Self, override
 
 BUFF_SIZE = 1024
 RESP_404 = b"HTTP/1.1 404 Not Found\r\n\r\n"
 RESP_200 = b"HTTP/1.1 200 OK\r\n\r\n"
+HOST = "localhost"
+PORT = 4221
+
+# Initialize Logger
+logging.basicConfig(
+    level=logging.INFO,  # Change to DEBUG for more details
+    format="%(asctime)s %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+logger = logging.getLogger(__name__)
 
 
 class HttpReqLine:
@@ -82,13 +95,13 @@ class HttpRequest:
 
         return cls(req_line, headers, last)
 
+
 def echo_handler(text: str):
     headers = HttpHeaders(
         {"Content-Type": "text/plain", "Content-Length": str(len(text))}
     )
-    return b"\r\n".join(
-        [b"HTTP/1.1 200 OK", headers.to_bytes(), text.encode("utf-8")]
-    )
+    return b"\r\n".join([b"HTTP/1.1 200 OK", headers.to_bytes(), text.encode("utf-8")])
+
 
 def user_agent_handler(req: HttpRequest):
     user_agent = req.headers.get("User-Agent")
@@ -104,27 +117,56 @@ def user_agent_handler(req: HttpRequest):
     )
 
 
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client = server_socket.accept()[0]  # wait for client
+async def handle_client(client: socket.socket, loop: asyncio.AbstractEventLoop):
+    addr: tuple[str, int] = client.getpeername()  # (address, port) for AF_INET
+    logger.info(f"[CONNECTED] {addr[0]}:{addr[1]}")
 
-    req = client.recv(BUFF_SIZE).decode()
+    try:
+        while True:
+            req = await loop.sock_recv(client, BUFF_SIZE)
+            if not req:
+                break
 
-    http_request = HttpRequest.from_str(req)
+            req = req.decode()
+            logger.info(f"[RECEIVED from {addr[0]}:{addr[1]}] {req!r}")
 
-    match http_request.req_line.path.split("/")[1:]:
-        case [""]:
-            client.sendall(RESP_200)
+            http_request = HttpRequest.from_str(req)
 
-        case ["echo", text]:
-            client.sendall(echo_handler(text))
+            match http_request.req_line.path.split("/")[1:]:
+                case [""]:
+                    await loop.sock_sendall(client, RESP_200)
 
-        case ["user-agent"]:
-            client.sendall(user_agent_handler(http_request))
+                case ["echo", text]:
+                    await loop.sock_sendall(client, echo_handler(text))
 
-        case _:
-            client.sendall(RESP_404)
+                case ["user-agent"]:
+                    await loop.sock_sendall(client, user_agent_handler(http_request))
+
+                case _:
+                    await loop.sock_sendall(client, RESP_404)
+    except Exception as e:
+        logger.exception(f"[ERROR] {e}")
+
+    finally:
+        logger.info(f"[DISCONNECTED] {addr[0]}:{addr[1]}")
+        client.close()
+
+
+async def main():
+    server = socket.create_server((HOST, PORT), family=socket.AF_INET, reuse_port=True)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.setblocking(False)
+
+    loop = asyncio.get_running_loop()
+    logger.info(f"[SERVER STARTED] Listening on {HOST}:{PORT}")
+
+    while True:
+        sock = await loop.sock_accept(server)
+        client = sock[0]
+        client.setblocking(False)
+
+        _ = asyncio.create_task(handle_client(client, loop))
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
